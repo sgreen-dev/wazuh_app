1d6fb84329950a631dcfe318f34533a2ad7a1574
Shurron Green <shurron@protonmail.com>
2026-04-01 21:33:37 -0400

    Removed Hardcoded Values

--- compute.tf
+++ compute.tf
@@ -12,14 +12,14 @@ data "aws_ami" "ubuntu" {
 }
 resource "aws_instance" "web1" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.medium"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
 }
 resource "aws_instance" "web2" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.medium"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
